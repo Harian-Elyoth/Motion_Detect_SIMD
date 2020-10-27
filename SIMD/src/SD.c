310583b7ef9075fc8ff5df2d73d1702830ce5600
EnzoPolytech <enzocalvino3@gmail.com>
2020-10-27 17:37:08 +0100

    add SD file

---
+++ SIMD/src/SD.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "nrdef.h"
+#include "nrutil.h"
+
+#include "vnrdef.h"
+#include "vnrutil.h"
+
+#include "mutil.h"
+
+#include "mymacro.h"
+#include "simd_macro.h"
+
+/* Algorithme Sigma Delta pour le traitement d'image */
