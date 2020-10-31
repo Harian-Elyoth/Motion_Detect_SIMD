d653bd5db0b10b1e49d8e7d4da1e28f35785f580
EnzoPolytech <enzocalvino3@gmail.com>
2020-10-31 13:28:51 +0100

    refactoring

---
+++ codes/src/SD_simd.c
@@ -0,0 +1,32 @@
+/* ------------------------------------------------------------------ */
+/* ---  Algorithme Sigma Delta en SIMD pour le traitement d'image --- */
+/* ------------------------------------------------------------------ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "nrdef.h"
+#include "nrutil.h"
+
+#include "vnrdef.h"
+#include "vnrutil.h"
+
+#include "mutil.h"
+
+#include "mymacro.h"
+#include "simd_macro.h"
+
+#include "SD_simd.h"
+
+
+
+
+
+
+
+
+void main_SD_simd(int argc, char * argv[])
+{
+	
+	return 0;
+}
